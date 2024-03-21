ee7427389e27914d62f9de8b5a2c9284d0fcce2b
Alexandru <thealexandru2292@gmail.com>
2024-03-21 13:30:59 +0200

    Parameterized Url routing '/friends/:friendId'

--- server.js
+++ server.js
@@ -3,6 +3,18 @@ const express = require('express');
 const app = express();
 
 const PORT = 3000;
+
+const friends = [
+    {
+        id: 0,
+        name: 'Albert Einstein'
+    },
+    {
+        id: 1,
+        name: 'Sir Isaac Newton'
+    }
+];
+
 //express automatically sets the Content-Type for us, in this case will set to text/html; charset=utf-8
 
 /* app.get('/', (req, res) => { 
@@ -11,11 +23,24 @@ const PORT = 3000;
 
 //but in this case Express will set the Content-Type to application/json; charset=utf-8 for us/
 
-app.get('/', (req, res) => {
-    res.send({
-        id: 1,
-        name: 'Sir Isaac Newton'
-    });
+app.get('/friends', (req, res) => {
+    res.json(friends);
+})
+
+//GET /friends/22
+app.get('/friends/:friendId', (req, res) => {
+    const friendId = Number(req.params.friendId);
+    const friend = friends[friendId];
+    if(friend)
+    {
+        res.status(200).json(friend);
+    }
+    else
+    {
+        res.status(404).json({
+            error: "Friend does not exist"
+        });
+    }
 })
 
 app.get('/messages', (req, res) => {
