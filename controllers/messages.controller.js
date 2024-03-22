7b97463586c79d04be9d22020c0163054f51e950
Alexandru <thealexandru2292@gmail.com>
2024-03-22 18:10:48 +0200

    sending files via REST APIs

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -1,5 +1,13 @@
+const path = require('path');
+//we need to avoid the different ways on using paths on different operatying systems with PATH module
+// example on linux: /folder/file.jpg
+// example on windows: \folder\file.jpg
+
 function getMessages(req, res){
-    res.send('<ul> <li> Hello Albert!</li></ul>');
+    //__dirname = current folder
+    // '..' = one level up
+    res.sendFile(path.join(__dirname, '..', 'public', 'skymountain.webp'))
+    //res.send('<ul> <li> Hello Albert!</li></ul>');
 }
 
 function postMessage(req, res){
